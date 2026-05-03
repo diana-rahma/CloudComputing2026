53360f2d53cfaea56d50eefe11eafc38e854d939
dianarhmaw <dianarhm12@gmail.com>
2026-05-03 09:51:03 +0700

    profile

--- profile-app/app/page.tsx
+++ profile-app/app/page.tsx
@@ -4,18 +4,18 @@ export default function Home() {
       <div className="max-w-2xl w-full bg-white shadow-xl rounded-2xl p-8">
         <h1 className="text-4xl font-bold mb-4">Diana Rahmawati</h1>
         <p className="text-lg mb-2">
-          <span className="font-semibold">Student ID:</span> 2341720000
+          <span className="font-semibold">NIM:</span> 2341720162
         </p>
         <p className="text-lg mb-2">
-          <span className="font-semibold">Tech Interests:</span> Web Development,
-          UI/UX Design, Cloud Computing, IoT
+          <span className="font-semibold">Minat:</span> Web Development,
+          UI/UX Design, dan Data Analyst
         </p>
 
         <div className="mt-6">
           <h2 className="text-2xl font-semibold mb-2">About Me</h2>
           <p className="text-gray-700 leading-relaxed">
             I am an Informatics student passionate about building modern web
-            applications and exploring cloud-based systems. I enjoy creating
+            applications and exploring about data. I enjoy creating
             user-friendly interfaces, solving real-world problems, and learning
             new technologies.
           </p>
@@ -24,9 +24,9 @@ export default function Home() {
         <div className="mt-6">
           <h2 className="text-2xl font-semibold mb-2">Projects</h2>
           <ul className="list-disc list-inside text-gray-700 space-y-1">
-            <li>Student Achievement Information System</li>
-            <li>Rideease - Student Motorcycle Rental App</li>
-            <li>IoT Smart Toll Gate Prototype</li>
+            <li>Sistem Prestasi Mahasiswa</li>
+            <li>Sistem Deteksi Pembuatan Batik</li>
+            <li>IoT Smart Toll Gate</li>
           </ul>
         </div>
       </div>
